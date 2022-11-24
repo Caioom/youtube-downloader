6fd637e71b5d1d39287a641b7353e79e5faa660c
caioom <caio.ds.2003@gmail.com>
2022-11-23 23:03:12 -0300

    creating upload file box

---
+++ src/DownloadFromFile.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Header from './Header.svelte'
+</script>
+<style>
+  .alignAsColumn {
+    flex-direction: column;
+  }
+
+  .centerDisplayFlex {
+    display: flex;
+    align-self: center;
+    justify-content: center;
+  }
+
+  .container {
+    height: 100vh;
+    width: 25%; 
+  }
+
+  #uploadFileInput {
+    display: none;
+  }
+
+  .uploadFileLabel {
+    cursor: pointer;
+    border: solid 1px #000;
+    width: 100%;
+    height: 25%;
+  }
+
+</style>
+<Header />
+<div class="container centerDisplayFlex alignAsColumn">
+  <p>Insert here your file here:</p>
+  <label for="uploadFileInput" class="uploadFileLabel centerDisplayFlex">
+    <input id="uploadFileInput" type="file"/>
+  </label>
+</div>
